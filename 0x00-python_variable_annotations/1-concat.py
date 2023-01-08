762bbdd093de88b50a6dbc23d8f1a0a88a961b75
VicvekSr <Olamideoso65@gmail.com>
2023-01-08 15:03:32 +0000

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This module contains concat function with variable annotations
+"""
+
+
+def concat(str1: str, str2: str):
+    """ concat function with variable annotations
+    """
+    return str1 + str2
