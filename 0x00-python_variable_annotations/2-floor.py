80e2b1a34b1163b5dc8d7785cd364a4041ef6d73
VicvekSr <Olamideoso65@gmail.com>
2023-01-08 15:12:12 +0000

    add a floor function with variable annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" This module contains the floor type variable annotations
+"""
+
+
+import math
+from math import floor
+
+
+def floor(n: float) -> float:
+    """ Returns the floor value of n
+    """
+    return math.floor(n)
