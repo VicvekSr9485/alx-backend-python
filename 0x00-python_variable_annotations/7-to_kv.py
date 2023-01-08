97ede64b5d368b81185810fe21057a6ddbcd8aed
VicvekSr <Olamideoso65@gmail.com>
2023-01-08 16:40:53 +0000

    fix pycodestyle error

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@
 from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float])  -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ returns a tuple of string and float
     """
     return (k, v**2)
