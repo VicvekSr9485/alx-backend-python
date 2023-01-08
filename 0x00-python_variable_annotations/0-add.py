567df2544c804c559798f6c80bbfe11a587e435e
VicvekSr <Olamideoso65@gmail.com>
2023-01-08 14:56:23 +0000

    add function add of variable annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+""" This module contains add function of variable annotations
+"""
+
+
+def add(a: float, b: float):
+    """ function add of variable annotation
+    """
+    return a + b
