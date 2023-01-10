5043e67cc30c2a6e3a493c2844e731836719c32b
VicvekSr <Olamideoso65@gmail.com>
2023-01-10 22:49:03 +0000

    measure the total runtime of four parallel coroutines

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -3,6 +3,7 @@
 """
 
 import asyncio
+import time
 
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
@@ -11,4 +12,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """ Measure the total runtime of four parallel coroutines
     """
-    
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end_time = time.time()
+    return end_time - start_time
