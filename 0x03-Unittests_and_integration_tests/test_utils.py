70f5a75403d21c71e2341259fbfa4530c428a261
VicvekSr <Olamideoso65@gmail.com>
2023-02-05 13:00:22 +0000

    add a parametrized test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,7 +10,7 @@ class TestAccessNestedMap(unittest.TestCase):
     """ TestAccessNestedMap Test Class """
     @parameterized.expand([
         ({"a": 1}, ("a"), 1),
-        ({"a": {"b": 2}}, ("a"), 2),
+        ({"a": {"b": 2}}, ("a"), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
